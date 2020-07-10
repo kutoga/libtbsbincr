ac943561dd4df416d960271a42a35f7ce4dceb15
Benjamin B. Meier <benjamin.meier70@gmail.com>
2020-07-10 23:34:18 +0200

    Add a simple encryption test

--- v2/tests/enc.cc
+++ v2/tests/enc.cc
@@ -6,6 +6,8 @@
 #define CODE_END_MARKER                                 _TBS_LABEL(_code_end)
 #define CODE_START                                      (const unsigned char*)&&_code_start
 #define CODE_END                                        (const unsigned char*)&&_code_end
+#define CODE_LEN                                        ((const unsigned char *)&&_code_end - (const unsigned char *)&&_code_start)
+
 
 static const unsigned char *c_memmem(const unsigned char *start_incl, const unsigned char *end_excl, const unsigned char *pattern, size_t pattern_size) {
     for (const unsigned char *curr = start_incl; curr + pattern_size < end_excl; ++curr) {
@@ -36,7 +38,21 @@ TEST(tbs_enc, encrypted_section_tags) {
 }
 
 TEST(tbs_enc, encryption_changes_code) {
-    ASSERT_FALSE(true);
+    int dummy = 0;
+    char *code_before = (char *)malloc(CODE_LEN);
+    char *code_after = (char *)malloc(CODE_LEN);
+    memcpy((void *)code_before, (void *)CODE_START, CODE_LEN);
+
+    CODE_START_MARKER;
+    tbs_enc({
+        dummy++; 
+    }, _.re_encrypt=true);
+    CODE_END_MARKER;
+
+    memcpy((void *)code_after, (void *)CODE_START, CODE_LEN);
+    EXPECT_TRUE(memcmp(code_before, code_after, CODE_LEN) != 0);
+    free(code_before);
+    free(code_after);
 }
 
 TEST(tbs_enc_exp, c_single_expression) {
